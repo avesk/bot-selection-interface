b020e2074ba91d6995993f6650ec1946002fc840
avesk <averykushner@gmail.com>
2020-07-16 20:22:30 -0700

    loops through bot subnet and pings the is_alive endpoint adding alive bots to live_bots payload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 from flask import Flask, render_template, Response, request
+import requests 
 import json
 
+bot_subnet = '10.0.0'
+bot_net_interval = (100, 255)
+bot_endpoint = '/is_active'
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,8 +14,15 @@ def homepage():
 
 @app.route("/live_bots", methods=['GET', 'POST'])
 def live_bots():
-    return Response(json.dumps(['127.0.0.1', '127.0.0.1', '127.0.0.1']),  mimetype='application/json')
-    # return Response(['127.0.0.1', '127.0.0.1', '127.0.0.1'])
+    active_bots = []
+    for i in range(*bot_net_interval):
+        bot_url = f"http://{bot_subnet}.{i}/{bot_endpoint}"
+        print(bot_url)
+        r = requests.get(url = bot_url) 
+        data = r.json()
+        if 'yes' in data:
+            active_bots += [bot_url]
+    return Response(json.dumps(active_bots),  mimetype='application/json')
 
 
 if __name__ == "__main__":
